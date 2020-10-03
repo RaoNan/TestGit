a171565728f63f507ce3fb7d09252be7ecc0ae18
CODE1/310301274 <ashwini.k@philips.com>
2020-10-03 23:00:18 +0530

    first commit

--- print.py
+++ print.py
@@ -5,8 +5,8 @@ import os
 print(sys.argv[1])
 print(sys.argv[2])
 print(sys.argv[3])
-print(os.getenv('praveen'))
-print(os.getenv('sunil'))
+print(os.getenv('test'))
+print(os.getenv('test1'))
 
 
 f= open(os.getenv('MODEL_PATH') + "/model.pl","w+")
